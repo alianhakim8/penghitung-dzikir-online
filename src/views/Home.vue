321ec168a06bad7d626f5b7316d6e77af53ea2b6
alianhakim8 <alianhakim9@outlook.com>
2021-08-18 16:06:14 +0700

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1>Aplikasi Penghitung Dzikir Online</h1>
+      <div class="row mt-5">
+        <div class="col-md-4">
+          <button class="btn btn-dark" @click="decrease()">-</button>
+        </div>
+        <div class="col-md-4">
+          <span class="btn btn-outline-dark">{{ counter }}</span>
+        </div>
+        <div class="col-md-4">
+          <button class="btn btn-dark" @click="increase()">+</button>
+        </div>
+      </div>
+      <button class="btn btn-danger mt-4" @click="reset()">
+        Ulangi dari 0
+      </button>
+      <!-- <p class="mt-4">alianhakim. 2021</p> -->
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  components: {},
+  methods: {
+    increase() {
+      this.counter++;
+    },
+    decrease() {
+      if (this.counter != 0) {
+        this.counter--;
+      }
+    },
+    reset() {
+      let yakin = confirm("Anda yakin ?");
+      if (yakin) {
+        this.counter = 0;
+      }
+    },
+  },
+};
 </script>
+
+<style scoped>
+</style>
